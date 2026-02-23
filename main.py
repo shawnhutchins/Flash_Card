6939de6eec6a7b6d093a4e1d0c53e0223f4c1e06
shawn <shawnhutchins1@gmail.com>
2026-02-22 21:34:45 -0500

    Added images list and set window background color

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from tkinter import *
 #Constants
 BACKGROUND_COLOR = "#B1DDC6"
 
+images = ["images/card_front.png",
+          "images/card_back.png",
+          "images/right.png",
+          "images/wrong.png"]
+
 window = Tk()
 window.title("Flash Card")
-window.configure(padx=50, pady=50)
+window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # ------------------------------ User Interface --------------------------- #
 
