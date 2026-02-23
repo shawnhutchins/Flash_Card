2feb1dfb1eec74d518f14b61e6e8888c5fca5234
Shawn <shawnhutchins1@gmail.com>
2026-02-22 19:52:58 -0500

    Added window padding and background color constant

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 # ---------------------------------- Tasks -------------------------------- #
 
+#Constants
+BACKGROUND_COLOR = "#B1DDC6"
+
 window = Tk()
 window.title("Flash Card")
+window.configure(padx=50, pady=50)
 
 # ------------------------------ User Interface --------------------------- #
 
