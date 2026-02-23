b4cf2233f8623894f42171c2e1fa3f0e57f8e47c
Shawn <shawnhutchins1@gmail.com>
2026-02-22 22:38:00 -0500

    Updated flashcard_initial_image to use the images list

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ window.title("Flash Card")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 flashcard_canvas = Canvas(master=window, width=800, height=526, bg=BACKGROUND_COLOR, borderwidth=0, highlightthickness=0)
-flashcard_initial_image = PhotoImage(file="images/card_front.png")
+flashcard_initial_image = PhotoImage(file=images[0])
 flashcard_canvas.create_image(400, 263, image=flashcard_initial_image)
 
 # ---------------------------------- Layout ------------------------------- #
