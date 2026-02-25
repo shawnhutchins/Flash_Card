1343b95c89144289ca1b80b73ed9617e64391f28
Shawn <shawnhutchins1@gmail.com>
2026-02-24 22:01:28 -0500

    Removed border width 0 from buttons and flashcard canvas

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ window = Tk()
 window.title("Flash Card")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flashcard_canvas = Canvas(master=window, width=800, height=526, bg=BACKGROUND_COLOR, borderwidth=0, highlightthickness=0)
+flashcard_canvas = Canvas(master=window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flashcard_initial_image = PhotoImage(file=images[0])
 flashcard_canvas.create_image(400, 263, image=flashcard_initial_image)
 
@@ -25,10 +25,10 @@ language_text = flashcard_canvas.create_text(400, 150, text="Language", font=("A
 word_text = flashcard_canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
 wrong_button_image = PhotoImage(file=images[2])
-wrong_button = Button(master=window, image=wrong_button_image, borderwidth=0, highlightthickness=0, bg=BACKGROUND_COLOR)
+wrong_button = Button(master=window, image=wrong_button_image, highlightthickness=0, bg=BACKGROUND_COLOR)
 
 right_button_image = PhotoImage(file=images[3])
-right_button = Button(master=window, image=right_button_image, borderwidth=0, highlightthickness=0, bg=BACKGROUND_COLOR)
+right_button = Button(master=window, image=right_button_image, highlightthickness=0, bg=BACKGROUND_COLOR)
 
 # ---------------------------------- Layout ------------------------------- #
 
