8a82dbd65b3fa93ba3ca39481896af6e89464b43
Shawn <shawnhutchins1@gmail.com>
2026-03-01 19:40:11 -0500

    Updating text values on flashcard canvas with new random word on button press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 import pandas
 
 # ---------------------------------- Tasks -------------------------------- #
@@ -14,14 +15,25 @@ images = ["images/card_front.png",
 
 dataframe = pandas.read_csv("data/french_words.csv")
 data = dataframe.to_dict(orient="records")
-print(data)
+current_word ={}
+
+#Sets the current_word global to a new random word from the data list
+def new_random_word():
+    global current_word
+    current_word = random.choice(data)
+
+#Updates the text values on the canvas with a new random word
+def update_flashcard_front():
+    new_random_word()
+    flashcard_canvas.itemconfig(language_text, text="French")
+    flashcard_canvas.itemconfig(word_text, text=current_word["French"])
 
 # ------------------------------ User Interface --------------------------- #
-
 window = Tk()
 window.title("Flash Card")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+#Canvas
 flashcard_canvas = Canvas(master=window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flashcard_initial_image = PhotoImage(file=images[0])
 flashcard_canvas.create_image(400, 263, image=flashcard_initial_image)
@@ -29,14 +41,18 @@ flashcard_canvas.create_image(400, 263, image=flashcard_initial_image)
 language_text = flashcard_canvas.create_text(400, 150, text="Language", font=("Ariel", 40, "italic"))
 word_text = flashcard_canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
+#Initializing flashcard_canvas text
+update_flashcard_front()
+
+#Buttons
 wrong_button_image = PhotoImage(file=images[2])
-wrong_button = Button(master=window, image=wrong_button_image, highlightthickness=0, bg=BACKGROUND_COLOR)
+wrong_button = Button(master=window, image=wrong_button_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=update_flashcard_front)
 
 right_button_image = PhotoImage(file=images[3])
-right_button = Button(master=window, image=right_button_image, highlightthickness=0, bg=BACKGROUND_COLOR)
+right_button = Button(master=window, image=right_button_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=update_flashcard_front)
 
 # ---------------------------------- Layout ------------------------------- #
-
+#Canvas
 flashcard_canvas.grid(row=0, column=0, columnspan=2)
 
 #Buttons
